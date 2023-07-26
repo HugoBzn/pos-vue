6908c771d9561ff03f1c3ca11203436a126c72c3
HugoBzn <hugobazan1499@gmail.com>
2023-07-25 19:53:41 -0600

    Form ready

--- formkit.config.js
+++ formkit.config.js
@@ -9,13 +9,13 @@ const config = {
         wrapper: 'space-y-2 mb-3',
         input: 'w-full p-3 border border-gray-300 rounded bg-white bg-opacity-50 text-black',
       },
-      text: {},
       file: {
         noFiles: 'block my-2',
         fileItem: 'hidden',
       },
-      number: {},
-      select: {},
+      submit: {
+        input: '$reset bg-green-500 hover:bg-green-700 w-full p-2 font-bold uppercase disabled:opacity-50',
+      },
     }),
   },
 };
