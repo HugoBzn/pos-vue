6c4caa6531706f64f86ea593c4f03c94908d617e
HugoBzn <hugobazan1499@gmail.com>
2023-07-25 19:27:37 -0600

    Styles to form

--- formkit.config.js
+++ formkit.config.js
@@ -5,12 +5,15 @@ const config = {
     classes: generateClasses({
       global: {
         label: 'block mb-1 font-bold text-lg',
-        input: 'text-black',
-        message: 'text-red-500 mb-2',
+        message: 'text-red-500 mb-2 font-bold',
         wrapper: 'space-y-2 mb-3',
+        input: 'w-full p-3 border border-gray-300 rounded bg-white bg-opacity-50 text-black',
       },
       text: {},
-      file: {},
+      file: {
+        noFiles: 'block my-2',
+        fileItem: 'hidden',
+      },
       number: {},
       select: {},
     }),
