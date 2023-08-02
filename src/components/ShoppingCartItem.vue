3d75bfc76e20eefe457ba0d7e76eb5aeb135091f
HugoBzn <hugobazan1499@gmail.com>
2023-08-02 15:18:57 -0600

    New select to products and limit to 5 or available

--- src/components/ShoppingCartItem.vue
+++ src/components/ShoppingCartItem.vue
@@ -1,5 +1,9 @@
 <script setup>
+import { useCartStore } from '../stores/cart';
 import { formatCurrency } from '../helpers/index';
+
+const cart = useCartStore();
+
 defineProps({
   item: {
     type: Object,
@@ -9,10 +13,15 @@ defineProps({
 
 <template>
   <li class="flex space-x-6 py-3">
-    <img :src="item.image" :alt="'Imagen de ' + item.name" class="h-24 w-24 flex-none rounded-xl" />
+    <img :src="item.image" :alt="'Imagen de ' + item.name" class="h-32 w-32 flex-none rounded-xl" />
     <div class="flex-auto space-y-2">
       <h3 class="font-bold">{{ item.name }}</h3>
       <p class="font-semibold">{{ formatCurrency(item.price) }}</p>
+      <select class="w-24 h-10 text-center p-2 rounded-lg bg-slate-100 bg-opacity-20 shadow">
+        <option v-for="n in cart.checkProductAvailability(item)" :value="n" class="text-black">
+          {{ n }}
+        </option>
+      </select>
     </div>
   </li>
 </template>
