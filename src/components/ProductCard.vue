ff231cb5be66dcffeb9dee35a5d4d52a096571a2
HugoBzn <hugobazan1499@gmail.com>
2023-08-02 12:46:52 -0600

    More css to products with card and btn

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { formatCurrency } from '../helpers/index';
+
 defineProps({
   product: {
     type: Object,
@@ -7,7 +9,30 @@ defineProps({
 </script>
 
 <template>
-  <div class="rounded shadow relative">
-    <img :src="product.image" :alt="product.image" class="rounded-3xl" />
+  <div class="rounded-xl bg-slate-100 bg-opacity-20 shadow relative">
+    <img :src="product.image" :alt="'Imagen de ' + product.name" class="rounded-b-3xl rounded-t-xl" />
+
+    <div class="p-3 space-y-2">
+      <h3 class="text-2xl font-extrabold">{{ product.name }}</h3>
+      <p>Disponibles: {{ product.availability }}</p>
+      <p class="text-xl font-semibold">{{ formatCurrency(product.price) }}</p>
+    </div>
+
+    <button type="button" class="absolute top-5 -right-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-8 h-8 bg-indigo-600 rounded-full"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    </button>
   </div>
 </template>
