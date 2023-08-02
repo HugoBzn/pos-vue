505f1bfc03bfcd687041ea9fa7e3d787db6a198f
HugoBzn <hugobazan1499@gmail.com>
2023-08-02 14:15:54 -0600

    New component to list products in cart New style

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,4 +1,5 @@
 <script setup>
+import ShoppingCartItem from './ShoppingCartItem.vue';
 import { useCartStore } from '../stores/cart';
 
 const cart = useCartStore();
@@ -6,4 +7,11 @@ const cart = useCartStore();
 
 <template>
   <p v-if="cart.isEmpty" class="text-xl text-center font-bold">El carrito está vacío</p>
+
+  <div v-else>
+    <p class="text-4xl font-bold">Resumen de venta</p>
+    <ul role="list" class="mt-6 divide-y divide-gray-200">
+      <ShoppingCartItem v-for="item in cart.items" :key="item.id" :item="item" />
+    </ul>
+  </div>
 </template>
---
+++ src/components/ShoppingCartItem.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { formatCurrency } from '../helpers/index';
+defineProps({
+  item: {
+    type: Object,
+  },
+});
+</script>
+
+<template>
+  <li class="flex space-x-6 py-3">
+    <img :src="item.image" :alt="'Imagen de ' + item.name" class="h-24 w-24 flex-none rounded-xl" />
+    <div class="flex-auto space-y-2">
+      <h3 class="font-bold">{{ item.name }}</h3>
+      <p class="font-semibold">{{ formatCurrency(item.price) }}</p>
+    </div>
+  </li>
+</template>
