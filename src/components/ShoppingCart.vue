3342dd5faceef57bb86b32846d55af6ee2dbf43c
HugoBzn <hugobazan1499@gmail.com>
2023-08-02 15:56:07 -0600

    New component with slots to print data

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,6 +1,8 @@
 <script setup>
 import ShoppingCartItem from './ShoppingCartItem.vue';
 import { useCartStore } from '../stores/cart';
+import { formatCurrency } from '../helpers';
+import Amount from './Amount.vue';
 
 const cart = useCartStore();
 </script>
@@ -10,8 +12,24 @@ const cart = useCartStore();
 
   <div v-else>
     <p class="text-4xl font-bold">Resumen de venta</p>
+
     <ul role="list" class="mt-6 divide-y divide-gray-200">
       <ShoppingCartItem v-for="item in cart.items" :key="item.id" :item="item" />
     </ul>
+
+    <dl class="pt-2 mt-6 border-t border-gray-200 text-sm md:text-base font-medium text-gray-200">
+      <Amount>
+        <template #label>Subtotal:</template>
+        {{ formatCurrency(300) }}
+      </Amount>
+      <Amount>
+        <template #label>Impuestos:</template>
+        {{ formatCurrency(0) }}
+      </Amount>
+      <Amount>
+        <template #label>Total a pagar:</template>
+        {{ formatCurrency(0) }}
+      </Amount>
+    </dl>
   </div>
 </template>
