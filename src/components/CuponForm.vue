102533a0b9cb1d2c1beaf26baafab85c38dd0f4e
HugoBzn <hugobazan1499@gmail.com>
2023-08-04 18:08:37 -0600

    Creating a store to save data in coupon

--- src/components/CuponForm.vue
+++ src/components/CuponForm.vue
@@ -1,4 +1,8 @@
-<script setup></script>
+<script setup>
+import { useCouponsStore } from '../stores/coupons';
+
+const couponStore = useCouponsStore();
+</script>
 
 <template>
   <p class="mt-5 py-5 font-bold border-t">¿Tienes un cupón?</p>
@@ -8,7 +12,10 @@
       type="text"
       placeholder="Ingresa un cupón"
       class="p-2 border-gray-200 rounded-lg rounded-r-none w-full"
+      v-model="couponStore.couponInput"
     />
-    <button type="button" class="p-3 bg-green-500 hover:bg-green-600 font-bold rounded-lg rounded-l-none">Canjear</button>
+    <button type="button" class="p-3 bg-green-500 hover:bg-green-600 font-bold rounded-lg rounded-l-none">
+      Canjear
+    </button>
   </div>
 </template>
---
+++ src/stores/coupons.js
@@ -0,0 +1,10 @@
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+
+export const useCouponsStore = defineStore('coupon', () => {
+  const couponInput = ref('');
+
+  return {
+    couponInput,
+  };
+});
