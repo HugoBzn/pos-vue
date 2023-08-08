b3def02f02d6b80553c1676c54445ac0a65abca3
HugoBzn <hugobazan1499@gmail.com>
2023-08-08 13:51:32 -0600

    Install vue-calendar-datepicker Added to tailwind-config

--- src/views/admin/SalesView.vue
+++ src/views/admin/SalesView.vue
@@ -1,7 +1,20 @@
-<script setup></script>
+<script setup>
+import VueTailwindDatePicker from 'vue-tailwind-datepicker';
+</script>
 
 <template>
   <div class="px-5">
-    <h1 class="text-4xl my-10">Resumen de Ventas</h1>
+    <h1 class="text-4xl my-5">Resumen de Ventas</h1>
+
+    <div class="md:flex md:items-start gap-5">
+      <div class="md:w-1/2 lg:w-1/3 flex justify-center">
+        <VueTailwindDatePicker 
+          class="text-black"
+        />
+      </div>
+      <div class="md:w-1/2 lg:w-2/3 space-y-5 lg:h-screen lg:overflow-y-auto p-5 pb-32">
+        <p>Ventas aquí</p>
+      </div>
+    </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+console.log(colors);
+
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './formkit.config.js'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './formkit.config.js',
+    './node_modules/vue-tailwind-datepicker/**/*.js',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'vtd-primary': colors.green,
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
